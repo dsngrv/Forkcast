4d5a77747d835f812bff9ca5ae991db0b8a186eb
dsngrv <123954612+dsngrv@users.noreply.github.com>
2024-05-27 17:26:12 +0400

    Initial Commit

---
+++ Forkcast/ForkcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForkcastApp.swift
+//  Forkcast
+//
+//  Created by Дмитрий Снигирев on 27.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ForkcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
